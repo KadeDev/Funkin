bab14e74e8e5870c6f1098f05f4925d61b325c13
CyndaquilDAC <65635932+CyndaquilDAC@users.noreply.github.com>
2021-04-14 21:11:32 -0500

    swagshit

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -25,6 +25,8 @@ class HealthIcon extends FlxSprite
 		animation.add('mom-car', [6, 7], 0, false, isPlayer);
 		animation.add('tankman', [8, 9], 0, false, isPlayer);
 		animation.add('face', [10, 11], 0, false, isPlayer);
+		animation.add('dad-test', [10, 11], 0, false, isPlayer);
+		animation.add('bf-test', [10, 11], 0, false, isPlayer);
 		animation.add('dad', [12, 13], 0, false, isPlayer);
 		animation.add('senpai', [22, 28], 0, false, isPlayer);
 		animation.add('senpai-angry', [22, 28], 0, false, isPlayer);
