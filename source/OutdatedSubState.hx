943004d356305dbc99119b5f04807245ee95313a
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2021-04-29 00:35:20 +0700

    upload mp3 versions of the remixed songs so it works on html5 version

--- source/OutdatedSubState.hx
+++ source/OutdatedSubState.hx
@@ -19,7 +19,7 @@ class OutdatedSubState extends MusicBeatState
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 		var txt:FlxText = new FlxText(0, 0, FlxG.width,
-			"Kade Engine is Outdated!\n"
+			"Cappy's Mix is Outdated!\n"
 			+ MainMenuState.cappyMixVer
 			+ " is your current version\nwhile the most recent version is " + needVer
 			+ "!\nPress Space to go to the github or ESCAPE to ignore this!!",
